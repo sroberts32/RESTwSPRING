96ef3d01d0a43914b1e374d8bd8afdd7028e92dd
unknown <crimm_32@hotmail.com>
2022-06-01 13:06:07 -0400

    first commit

---
+++ src/main/java/com/example/payroll/EmployeeNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.payroll;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+    EmployeeNotFoundException(Long id) {
+        super("Could not find employee " + id);
+    }
+}
